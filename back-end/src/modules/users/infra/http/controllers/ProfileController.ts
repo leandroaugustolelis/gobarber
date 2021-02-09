a4e473030e4784ca254bbacaacd8cf06fed8a552
Leandro Lelis <leandroaugustolelis@gmail.com>
2021-02-09 02:52:00 -0300

    finished profile routes and controllers

--- back-end/src/modules/users/infra/http/controllers/ProfileController.ts
+++ back-end/src/modules/users/infra/http/controllers/ProfileController.ts
@@ -1,34 +1,36 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
-import ShowProfileService from '@modules/users/services/ShowProfileService';
 import UpdateProfileService from '@modules/users/services/UpdateProfileService';
+import ShowProfileService from '@modules/users/services/ShowProfileService';
 
-export default class ProfileController {
+class ProfileController {
   public async show(request: Request, response: Response): Promise<Response> {
-    const user_id = request.user.id;
+    const { id: user_id } = request.user;
 
     const showProfile = container.resolve(ShowProfileService);
 
     const user = await showProfile.execute({ user_id });
 
-    return response.json(user);
+    return response.status(200).json(user);
   }
 
   public async update(request: Request, response: Response): Promise<Response> {
-    const user_id = request.user.id;
-    const { name, email, old_password, password } = request.body;
+    const { email, name, oldPassword, password } = request.body;
+    const { id: user_id } = request.user;
 
     const updateProfile = container.resolve(UpdateProfileService);
 
-    const user = await updateProfile.execute({
+    const updatedUser = await updateProfile.execute({
       user_id,
-      name,
       email,
-      old_password,
+      name,
+      oldPassword,
       password,
     });
 
-    return response.json(user);
+    return response.status(200).json(updatedUser);
   }
 }
+
+export default ProfileController;
