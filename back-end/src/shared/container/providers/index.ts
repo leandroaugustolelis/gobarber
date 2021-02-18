fc3ec5024149e368b8953f82628a299152ac77ea
Leandro Lelis <leandroaugustolelis@gmail.com>
2021-02-18 03:17:54 -0300

    finish cache storage

--- back-end/src/shared/container/providers/index.ts
+++ back-end/src/shared/container/providers/index.ts
@@ -1,4 +1,39 @@
-import './StorageProvider';
-import './MailTemplateProvider';
-import './MailProvider';
-import './CacheProvider';
+import { container } from 'tsyringe';
+import mailConfig from '@config/mail';
+
+import uploadConfig from '@config/upload';
+import IStorageProvider from './StorageProvider/models/IStorageProvider';
+import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
+import S3StorageProvider from './StorageProvider/implementations/S3StorageProvider';
+
+import IMailProvider from './MailProvider/models/IMailProvider';
+import SESMailProvider from './MailProvider/implementations/SESMailProvider';
+
+import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
+import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
+
+import ICacheProvider from './CacheProvider/models/ICacheProvider';
+import RedisCacheProvider from './CacheProvider/implementations/RedisCacheProvider';
+import EtheralMailProvider from './MailProvider/implementations/EtheralMailProvider';
+
+container.registerSingleton<IStorageProvider>(
+  'StorageProvider',
+  uploadConfig.driver === 'disk' ? DiskStorageProvider : S3StorageProvider
+);
+
+container.registerSingleton<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  HandlebarsMailTemplateProvider
+);
+
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  mailConfig.driver === 'ethereal'
+    ? container.resolve(EtheralMailProvider)
+    : container.resolve(SESMailProvider)
+);
+
+container.registerSingleton<ICacheProvider>(
+  'CacheProvider',
+  RedisCacheProvider
+);
