12cda921d6a455bc31193db233da21a0227cf09b
Leandro Lelis <leandroaugustolelis@gmail.com>
2021-02-20 04:51:08 -0300

    fixing Amazon SES mail sending

--- back-end/src/config/mail.ts
+++ back-end/src/config/mail.ts
@@ -1,7 +1,21 @@
-interface IMalConfig {
+import { MAIL_DRIVER, MAIL_OWNER, MAIL_DOMAIN } from '@shared/utils/enviroment';
+
+interface IMailConfig {
   driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      name: string;
+      email: string;
+    };
+  };
 }
 
 export default {
-  driver: process.env.MAIL_DRIVER || 'ethereal',
-} as IMalConfig;
+  driver: MAIL_DRIVER || 'ethereal',
+  defaults: {
+    from: {
+      name: MAIL_OWNER,
+      email: MAIL_DOMAIN,
+    },
+  },
+} as IMailConfig;
--- back-end/src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
+++ back-end/src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
@@ -1,17 +1,34 @@
+import { inject, injectable } from 'tsyringe';
+import mailConfig from '@config/mail';
+import aws from 'aws-sdk';
 import nodemailer, { Transporter } from 'nodemailer';
-import { injectable, inject } from 'tsyringe';
-import IMailTemplateProvider from '../../MailTemplateProvider/models/IMailTemplateProvider';
-import ISendMailDTO from '../dtos/ISendMailDTO';
+import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
 import IMailProvider from '../models/IMailProvider';
+import ISendMailDTO from '../dtos/ISendMailDTO';
 
 @injectable()
-export default class SESMailProvider implements IMailProvider {
-  private client: Transporter;
+class SESMailProvider implements IMailProvider {
+  private transporter: Transporter;
+
+  private mailTemplateProvider: IMailTemplateProvider;
 
   constructor(
     @inject('MailTemplateProvider')
-    private mailTemplateProvider: IMailTemplateProvider
-  ) {}
+    mailTemplateProvider: IMailTemplateProvider
+  ) {
+    aws.config.update({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      region: 'us-east-2',
+    });
+
+    this.mailTemplateProvider = mailTemplateProvider;
+    this.transporter = nodemailer.createTransport({
+      SES: new aws.SES({
+        apiVersion: '2010-12-01',
+      }),
+    });
+  }
 
   public async sendMail({
     to,
@@ -19,6 +36,21 @@ export default class SESMailProvider implements IMailProvider {
     subject,
     templateData,
   }: ISendMailDTO): Promise<void> {
-    console.log('Funcionou');
+    const { name, email } = mailConfig.defaults.from;
+
+    await this.transporter.sendMail({
+      from: {
+        name: from?.name || name,
+        address: from?.email || email,
+      },
+      to: {
+        name: to.name,
+        address: to.email,
+      },
+      subject,
+      html: await this.mailTemplateProvider.parse(templateData),
+    });
   }
 }
+
+export default SESMailProvider;
